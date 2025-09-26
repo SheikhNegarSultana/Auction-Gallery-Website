2a65509c64f9cb5dc28a50abfcc7b266e41f0953
SheikhNegarSultana <sultanashashi64@gmail.com>
2025-09-27 00:30:16 +0600

    Disable ❤️ Button After Click Done

--- src/Components/Auctions Section/Auction.jsx
+++ src/Components/Auctions Section/Auction.jsx
@@ -1,11 +1,13 @@
-import React, { useId } from 'react';
-import { GoHeart } from "react-icons/go";
+import React, { useId, useState } from 'react';
+import { GoHeart,GoHeartFill  } from "react-icons/go";
 import { ToastContainer, toast } from 'react-toastify';
 
 
 const Auction = ({ item, handleFavCount }) => {
 
     const { image, title, currentBidPrice, timeLeft, bidsCount } = item
+
+    // React Toastify
 
     const favItemListDetails = { image, title, currentBidPrice, bidsCount }
 
@@ -14,6 +16,11 @@ const Auction = ({ item, handleFavCount }) => {
     const notify = () => toast(
         <h1 className=' font-medium text-white text-[1rem]'>Item Is Added To Your Favorite Lists</h1>
         , { containerId });
+
+
+    // Button Disable
+    const [clicked, setClicked] = useState(false)
+    
     
 
     return (
@@ -26,12 +33,27 @@ const Auction = ({ item, handleFavCount }) => {
                 </td>
                 <td className=" px-11 py-3 text-[#0E2954] font-medium">${currentBidPrice}</td>
                 <td className="px-4 py-3 font-medium text-[#0E2954] ">{timeLeft}</td>
-                <td className="px-9 cursor-pointer py-3 text-[1.4rem] text-[#0E2954]">
-                    <GoHeart 
-                    onClick={() =>{handleFavCount(favItemListDetails) ; notify()} }
-                     />
 
-                    <ToastContainer
+                <td className="px-9 py-3 text-[1.4rem]">
+
+                <button
+
+                    className={`${clicked? " text-red-500 cursor-not-allowed" : "text-[#0E2954] cursor-pointer"} `}
+
+                    disabled={clicked}
+
+                    onClick={() =>
+                    {handleFavCount(favItemListDetails) ; 
+                    notify();
+                    setClicked(true);} }>
+
+                    
+                    {clicked? <GoHeartFill /> : <GoHeart /> }
+
+                </button>
+
+
+                <ToastContainer
                         containerId={containerId}  
                         position="top-right"
                         autoClose={5000}
@@ -41,9 +63,8 @@ const Auction = ({ item, handleFavCount }) => {
                         pauseOnFocusLoss
                         draggable
                         theme="dark"
-                        
                    />
-                     
+
                 </td>
             </tr>
 
